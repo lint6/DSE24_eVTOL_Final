c9d28df06114f53cfd4d8103b059f441bd76f624
MosBurger99 <yunjae0901@gmail.com>
2024-12-17 15:14:24 +0100

    Updating method :))

--- FlightModel/Newtonian/misc.py
+++ FlightModel/Newtonian/misc.py
@@ -14,10 +14,9 @@ All
 
 '''
 
-import numpy as np
-
 
 def SCfunc_ForceVector(force_in):
+    import numpy as np
     if type(force_in) == float or type(force_in) == int:
         raise ValueError('SCfunc_ForceVector: Non-vector force encountered')
     force_in = np.array(force_in)
@@ -26,4 +25,28 @@ def SCfunc_ForceVector(force_in):
     return magnitude, unit_vector
 
 def SCfunc_EulerRotation():
+    import numpy as np 
+    from scipy.spatial.transform import Rotation  as R 
+
+    delta_euler_angle = [input("yaw angle"), input("pitch angle"), input("roll angle") ]
+
+    rotation_sequence = "zyx"
+
+    rotation = R.from_euler(rotation_sequence, delta_euler_angle, degrees=True)
+
+    rotation_matrix = rotation.as_matrix()
+
+    print ("rotation matrix is", rotation_matrix)
+
+    # applying rotation to my vector 
+    local_vector = np.array([1,1,1])
+    transformed_vector = rotation.apply(local_vector)
+    print("It was", local_vector, "but now in global axis its", transformed_vector)
+
+    #bring it back to normal 
+    inverse_rotation = rotation.inv()
+    inverse_transformed_vector = inverse_rotation.apply(transformed_vector)
+    print("Inverting the matrix back to local axis system", inverse_transformed_vector)
     
+
+
