a13c82ee15ea91a739dacf686994bc007361e7de
Justin Weernink <128027843+justinthelama@users.noreply.github.com>
2024-12-18 11:29:53 +0100

    more accurate usage of distributed load (based on weight) + implementation of deflection (& its angle)

--- Classes/NVM.py
+++ Classes/NVM.py
@@ -4,25 +4,44 @@ import matplotlib.pyplot as plt
 
 class NVM:
 
-    def __init__(self, length=3, thrust_load=1000, distributed_load=300):
+    def __init__(self, length=3, rho=2710, area=0.1, E=70, I=4 * 10**-5, thrust_load=1000):
         ''' Thrust acts upwards at the end of the beam , distributed load acts downwards along the entire length of the beam
         Note that the beam itself is fixed at one end, and free on the other end '''
-        self.length = length 
-        self.thrust_load = thrust_load 
-        self.distributed_load = distributed_load 
+        self.length = length # m
+        self.rho = rho # kg / m^3
+        self.area = area # m^2
+        self.E = E * 10**9 # GPa
+        self.I = I # m^4
+        self.thrust_load = thrust_load # N
+        self.g = 9.80665 # m/s^2
+    
 
-        self.reaction_force = (self.distributed_load * self.length) - self.thrust_load
-        self.reaction_moment = (0.5 * self.distributed_load * self.length**2) - (self.thrust_load * self.length)
+
+        # calculations based on inputs
+        self.distributed_load = (self.rho * self.area * self.length * self.g) / self.length # N / m
+        self.reaction_force = (self.distributed_load * self.length) - self.thrust_load # N
+        self.reaction_moment = (0.5 * self.distributed_load * self.length**2) - (self.thrust_load * self.length) # Nm
 
     def shear_force(self, x: float):
         ''' Shear Force Calculation at a Distance 'x' from the fixed end '''
-        V = (-self.distributed_load * x) + self.reaction_force
-        return V
+        V = (-self.distributed_load * x) + self.reaction_force # N
+        return V 
     
     def bending_moment(self, x: float):
         ''' Bending Moment Calculation at a Distance 'x'  from the fixed end'''
         M = self.reaction_moment - (self.reaction_force*x) + (0.5*self.distributed_load*x**2)
         return M
+    
+    def deflection(self, x: float):
+        ''' Deflection Calculation at a Distance 'x' from the fixed end '''
+        v = (-1/(self.E * self.I)) * (-(self.distributed_load * x**4)/24 - (self.reaction_force * x**3)/6 + (self.reaction_moment * x**2)/2)
+        return v
+    
+    def deflection_angle(self, x: float):
+        ''' Deflection Angle Calculation at a Distance 'x' from the fixed end '''
+        theta = (-1/(self.E * self.I)) * (-(self.distributed_load * x**3)/6 - (self.reaction_force * x**2)/2 + (self.reaction_moment * x))
+        return theta
+
     
     def plot_NVM(self):
         ''' Plotting the Shear Force and Bending Moment Diagrams '''
@@ -57,19 +76,37 @@ class NVM:
         plt.tight_layout()
         plt.show()
 
+    def plot_deflection(self):
+        ''' Plotting the Shear Force and Bending Moment Diagrams '''
+        x = np.linspace(0, self.length, 1000)
+        deflections = [self.deflection(i) for i in x]
+        deflection_angles = [self.deflection_angle(i) for i in x]
+
+        plt.figure(figsize=(12, 6))
+
+        plt.subplot(1, 2, 1)
+        plt.plot(x, deflections, color='blue')
+        plt.axhline(y=0, color='black')
+        plt.title('Deflection Along the Beam')
+        plt.xlabel('Distance from Fixed End [m]')
+        plt.ylabel('Deflection Along the Beam [m]')
+        plt.grid(True)
+        
+        plt.subplot(1, 2, 2)
+        plt.plot(x, deflection_angles, color='red')
+        plt.axhline(y=0, color='black')
+        plt.title('Deflection Angle Along the Beam')
+        plt.xlabel('Distance from Fixed End [m]')
+        plt.ylabel('Deflection Angle [rad]')
+        plt.grid(True)
+
+        plt.tight_layout()
+        plt.show()
+
 
 if __name__ == '__main__':
     beam = NVM()
-
-    print(beam.shear_force(0))
-    print(beam.shear_force(1.25))
-    print(beam.shear_force(2.5))
-
-    print(beam.bending_moment(0))
-    print(beam.bending_moment(1.25))
-    print(beam.bending_moment(2.5))
-
-    beam.plot_NVM()
+    beam.plot_deflection()
 
 
 
