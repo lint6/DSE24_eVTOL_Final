42f197262d80175664dd2706f95a2dd0d052303a
Justin Weernink <128027843+justinthelama@users.noreply.github.com>
2024-12-18 17:30:02 +0100

    added the visual for stress distribution along the shaft (I-beam version)

--- Classes/NVM.py
+++ Classes/NVM.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 
 class NVM:
 
-    def __init__(self, length=3, rho=2710, area=0.1, E=70, I=4 * 10**-5, thrust_load=1000):
+    def __init__(self, length=3, rho=2710, area=5200, E=70, I=35.6*10**6, thrust_load=1000):
         ''' Thrust acts upwards at the end of the beam , distributed load acts downwards along the entire length of the beam
         Note that the beam itself is fixed at one end, and free on the other end '''
+        ### input area in [mm^2], input E in [GPa], input I in [mm^4]
         self.length = length # m
         self.rho = rho # kg / m^3
-        self.area = area # m^2
+        self.area = area * (0.001)**2 # m^2
         self.E = E * 10**9 # GPa
-        self.I = I # m^4
+        self.I = I * (0.001)**4 # m^4
         self.thrust_load = thrust_load # N
         self.g = 9.80665 # m/s^2
     
@@ -42,6 +43,36 @@ class NVM:
         theta = (-1/(self.E * self.I)) * (-(self.distributed_load * x**3)/6 - (self.reaction_force * x**2)/2 + (self.reaction_moment * x))
         return theta
 
+    def maximum_values(self):
+        x = np.linspace(0, self.length, 1000)
+        shear_forces = [self.shear_force(i) for i in x]
+        bending_moments = [self.bending_moment(i) for i in x]
+        deflections = [self.deflection(i) for i in x]
+        deflection_angles = [self.deflection_angle(i) for i in x]
+        maximum = [max(shear_forces, key=abs), max(bending_moments, key=abs), max(deflections, key=abs), max(deflection_angles, key=abs)]
+        return maximum
+
+    def sigma_z(self, x=0.085, y=-0.100, Iyy=8.2*10**6, drag_force=1000):
+        max_Mx = self.maximum_values()[1]
+        max_My = drag_force * self.length
+        sigma = (max_Mx * y)/self.I - (max_My * x) / (Iyy * (0.001)**4)
+        print(sigma/10**6)
+        return sigma
+    
+    ###         CONFIG 1  (H-beam)      ###
+    # Top Box: 170 x 10 (width x thickness)
+    # Web: 180 x 10 (height x thickness)
+    # Lower Box: 170 x 10 (width x thickness)
+
+    def config1_top_box(self):
+        
+
+
+
+
+
+
+    ###     PLOTTING    ###
     
     def plot_NVM(self):
         ''' Plotting the Shear Force and Bending Moment Diagrams '''
@@ -79,8 +110,8 @@ class NVM:
     def plot_deflection(self):
         ''' Plotting the Shear Force and Bending Moment Diagrams '''
         x = np.linspace(0, self.length, 1000)
-        deflections = [self.deflection(i) for i in x]
-        deflection_angles = [self.deflection_angle(i) for i in x]
+        deflections = [self.deflection(i)*1000 for i in x] # converted to mm
+        deflection_angles = [self.deflection_angle(i)*180/np.pi for i in x] # converted to deg
 
         plt.figure(figsize=(12, 6))
 
@@ -89,7 +120,7 @@ class NVM:
         plt.axhline(y=0, color='black')
         plt.title('Deflection Along the Beam')
         plt.xlabel('Distance from Fixed End [m]')
-        plt.ylabel('Deflection Along the Beam [m]')
+        plt.ylabel('Deflection Along the Beam [mm]')
         plt.grid(True)
         
         plt.subplot(1, 2, 2)
@@ -97,7 +128,7 @@ class NVM:
         plt.axhline(y=0, color='black')
         plt.title('Deflection Angle Along the Beam')
         plt.xlabel('Distance from Fixed End [m]')
-        plt.ylabel('Deflection Angle [rad]')
+        plt.ylabel('Deflection Angle [deg]')
         plt.grid(True)
 
         plt.tight_layout()
@@ -106,7 +137,9 @@ class NVM:
 
 if __name__ == '__main__':
     beam = NVM()
+    beam.plot_NVM()
     beam.plot_deflection()
+    beam.sigma_z()
 
 
 
