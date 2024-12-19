6d823932aa198df682c215e7173b209dfd34c64d
MosBurger99 <yunjae0901@gmail.com>
2024-12-19 09:00:54 +0100

    updated minor variables

--- FlightModel/Newtonian/physics.py
+++ FlightModel/Newtonian/physics.py
@@ -65,25 +65,25 @@ def SCfunc_FlightSimulation(Run=True, dt=0.1):
             lat_acc_y = aircraft.force[1] / aircraft.mass
             lat_acc_z = aircraft.force[2] / aircraft.mass
             ang_acc_x = aircraft.moment[0] / aircraft.inertia[0]
-            ang_acc_y = aircraft.moment[1] / aircraft.inertia[]
-            ang_acc_z = 
+            ang_acc_y = aircraft.moment[1] / aircraft.inertia[1]
+            ang_acc_z = aircraft.moment[2] / aircraft.inertia[2]
             
             #get velocity
-            vel_x = 0
-            vel_y = 0
-            vel_z = 0
+            vel_x = aircraft.vel_x + lat_acc_x * dt
+            vel_y = aircraft.vel_y + lat_acc_y * dt
+            vel_z = aircraft.vel_z + lat_acc_z * dt 
             rot_x = 0
             rot_y = 0
             rot_z = 0
             
             #get position
-            pos_x = 0
-            pos_y = 0
-            pos_z = 0
+            pos_x += vel_x * dt
+            pos_y += vel_y * dt 
+            pos_z += vel_z * dt
             ang_x = 0
             ang_y = 0
             ang_z = 0
             
             #update forces
-            state = np.array 
             #log data
+            return pos_x, pos_y, pos_z
