e5ce34158348037b34332b225fbb91a8221f0bd8
flopetersen <florens.petersen@gmail.com>
2025-01-06 13:59:48 +0100

    Changed airfoile file reference for applicability across different computers

--- Aerodynamic analysis/airfoil.py
+++ Aerodynamic analysis/airfoil.py
@@ -4,7 +4,7 @@ import numpy as np
 class AirfoilData:
     def __init__(self):
         # Define the filename
-        self.filename = r'/Users/florenspetersen/Documents/GitHub/DSE24_eVTOL_Final/Aero/xf-n0012-il-500000.csv'
+        self.filename = r'xf-n0012-il-500000.csv'
 
         # Initialize lists to store the data
         self.alpha = []
