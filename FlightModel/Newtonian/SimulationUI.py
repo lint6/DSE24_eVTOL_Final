53c47fd9d3561121a43a4e4955281c6b201830de
lintongxuxu <xulintong3@gmail.com>
2025-01-07 16:27:29 +0100

    sim updates

--- FlightModel/Newtonian/SimulationUI.py
+++ FlightModel/Newtonian/SimulationUI.py
@@ -19,21 +19,21 @@ from physics import *
 import matplotlib.pyplot as plt
 
 aircraft = SCobj_Aircraft(points=SCfunc_VerifyAircraft(), position=[0,0,0], rotation=[0,0,0])
-log_state, log_forces, log_time, log_extras = SCfunc_FlightSimulation(aircraft, runtime=60)
+log_state, log_forces, log_time, log_extras, log_acc = SCfunc_FlightSimulation(aircraft, runtime=100)
 
 fig, axs = plt.subplots(5)
 
 
 axs[0].plot(log_time, -1*np.array(log_state[0]).T[2], label = 'Aircraft')
-axs[0].plot(log_time, log_extras[1], 'r', label = 'setpoint')
+# axs[0].plot(log_time, log_extras[1], 'r', label = 'setpoint')
 axs[0].set_ylabel('Height')
 
 axs[1].plot(log_time, np.array(log_state[1]).T[1], label = 'Aircraft')
-axs[1].plot(log_time, log_extras[2], 'r', label = 'setpoint')
+# axs[1].plot(log_time, log_extras[2], 'r', label = 'setpoint')
 axs[1].set_ylabel('Pitch')
 
 axs[2].plot(log_time, np.array(log_state[1]).T[0], label = 'Aircraft')
-axs[2].plot(log_time, log_extras[2], 'r', label = 'setpoint')
+# axs[2].plot(log_time, log_extras[2], 'r', label = 'setpoint')
 axs[2].set_ylabel('Roll')
 
 axs[3].plot(log_time, np.array(log_state[1]).T[2], label = 'Aircraft')
@@ -47,4 +47,8 @@ axs[4].set_ylabel('RPM (normalized)')
 
 axs[4].set_xlabel('time')
 
+plt.show()
+plt.clf
+
+plt.plot(log_time, log_acc[0][2])
 plt.show()
