57d141f79be60c8869ef76fbde36c3c428b98a21
MosBurger99 <yunjae0901@gmail.com>
2025-01-16 10:42:32 +0100

    intertia update

--- FlightModel/Newtonian/misc.py
+++ FlightModel/Newtonian/misc.py
@@ -113,9 +113,35 @@ def SCfunc_LinearRamp(x1, x2, x, value1, value2):
 
 def SCfunc_RPM2RadSec(RPM):
     return RPM/60 * 2 * np.pi
+
 def SCfunc_RadSec2RPM(omega):
     return omega * 60 / (2*np.pi)
-    
+
+def SCfunc_Inertia_Disc(m,l):
+    I_xx = 0
+    I_yy = 0 
+    I_zz = (1/12) * m * l**2 
+    return I_xx, I_yy, I_zz
+
+def SCfunc_Inertia_Cylinder(m, r, l): # z going through the circle surface, 
+    I_xx = ((1/4) * m * r**2) + ((1/12) * m * l**2)
+    I_yy = ((1/4) * m * r**2) + ((1/12) * m * l**2)
+    I_zz = ((1/2) * m * r**2)
+    return I_xx, I_yy, I_zz
+
+
+def SCfunc_Intertia_Box(m, w, d, h): # w = width(y), d= depth(x), h = height(z)
+    I_xx = ((1/12) * m * (w**2 + h**2))
+    I_yy = ((1/12) * m * (h**2 + d**2))
+    I_zz = ((1/12) * m * (w**2 + d**2))
+    return I_xx, I_yy, I_zz
+
+def SCfunc_Intertia_Sphere(m, r):
+    I_xx = (2/5) * m * r**2
+    I_yy = (2/5) * m * r**2
+    I_zz = (2/5) * m * r**2
+    return I_xx, I_yy, I_zz
+
 
 DEBUG = False
 if DEBUG:
