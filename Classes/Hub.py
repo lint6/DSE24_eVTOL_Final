5a39dec9337dea1911b7e0eb5cae173e6c2c667a
ntangsirisathi <chisanestjr@gmail.com>
2025-01-16 11:07:26 +0100

    Add material and weight calc

--- Classes/Hub.py
+++ Classes/Hub.py
@@ -1,21 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class HubStress:
+class HubStuff:
     
-    def __init__(self, g_0=9.80665, m_R=10, d=100, z=20, T=150, G=80, L=200, D=10, r_R=1.2, b=6):
+    def __init__(self, g_0=9.80665, r_R=1.2, b=6, m_R=10, d=270, z=50, G=44, rho=4.43, T=213, L=1312.23, D=15.66):
         # Initialize the HubStress Class
-        # Input m in [kg], d in [mm], T in [Nm], G in [GPa], L in [N], D in [N], R in [m], and b in [-]
+        # Input r_R in [m], b in [-], m_R (1 rotor) in [kg], d in [mm], z in [mm], T in [Nm], G in [GPa], rho in [g/cc], L (1 rotor) in [N], and D (1 rotor) in [N]
+        
+        "Material Selection"
+        # Aluminium 7075-T6: G = 26.9 [GPa], rho = 2.81 [g/cc]
+        # Ti-6Al-4V: G = 44 [GPa], rho = 4.43 [g/cc]
+        # Steel AlSl 4130: G = 80 [GPa], rho = 7.85 [g/cc]
+
         self.g_0 = g_0  # Acceleration due to gravity in [m/s^2]
-        self.W_R = m_R * g_0 # Weight of 1 blade in [N]
-        self.d = d / 1000  # Diameter of the hub in [m]
-        self.z = z / 1000  # Thickness of the hub in [m]
-        self.T = T  # Torque in [Nm]
-        self.G = G * 10**9  # Shear Modulus in [Pa]
-        self.L = L  # Lift in [N]
-        self.D = D  # Drag in [N]
         self.r_R = r_R  # Radius of the rotor in [m]
         self.b = b  # Number of blades [-]
+        self.W_R = (m_R * g_0) / self.b # Weight of 1 blade in [N]
+        self.d = d / 1000  # Diameter of the hub in [m]
+        self.z = z / 1000  # Thickness of the hub in [m]
+        self.G = G * 10**9  # Shear Modulus of the material in [Pa]
+        self.rho = rho * 1000  # Density of the material in [kg/m^3]
+        self.T = T  # Torque in [Nm]
+        self.L = L / self.b  # Lift of 1 blade in [N]
+        self.D = D / self.b  # Drag of 1 blade in [N]
+
         
     def calculate_polar_moment_of_inertia(self):
         return (np.pi / 32) * (self.d**4) # returns in [m^4]
@@ -46,17 +54,22 @@ class HubStress:
         r = self.d / 2
         return ((self.T - (self.b * M_D)) * r) / J  # returns in [Pa]
     
-    def calculate_bending_stress(self, r_i=50):
-        # Input location of bending stress (r_i) in [mm]
+    def calculate_bending_stress(self):
+        # Input location of bending stress (r_i) in [mm], default as the radius of the hub
+        r_i = self.d / 2
         I = self.calculate_moment_of_inertia()
         M_L = self.calculate_moment_lift()
         M_W = self.calculate_moment_weight()
         M_x = M_L - M_W
         self.r_i = r_i / 1000   # Location on the cross section along the hub's radius in [m]
-        return ((M_x * self.r_i) / I)   # returns in [Pa]
+        return ((M_x * r_i) / I)   # returns in [Pa]
+
+    def calculate_hub_mass(self):
+        return np.pi * (self.d**2 / 4) * self.z * self.rho  # returns in [kg]
+
 
 if __name__ == "__main__":
-    hub = HubStress()
+    hub = HubStuff()
     polar_moment_of_inertia = hub.calculate_polar_moment_of_inertia()
     moment_of_inertia = hub.calculate_moment_of_inertia()
     moment_lift = hub.calculate_moment_lift()
@@ -66,6 +79,7 @@ if __name__ == "__main__":
     twist_angle = hub.calculate_twist_angle()
     shear_stress = hub.calculate_shear_stress()
     bending_stress = hub.calculate_bending_stress()
+    hub_mass = hub.calculate_hub_mass()
     print(f"The polar moment of inertia (J) of the hub is {polar_moment_of_inertia} [m^4]")
     print(f"The moment of inertia (I) of the hub is {moment_of_inertia} [m^4]")
     print(f"The moment due to lift (M_L) of 1 blade is {moment_lift} [Nm]")
@@ -75,3 +89,4 @@ if __name__ == "__main__":
     print(f"The twist angle of the hub is {twist_angle} [rad] or {twist_angle * 180 / np.pi} [deg]")
     print(f"The maximum shear stress in the hub is {shear_stress / 10**6} [MPa]")
     print(f"The bending stress in the hub at r = {hub.r_i} [m] is {bending_stress / 10**6} [MPa]")
+    print(f"The mass of the hub is {hub_mass} [kg]")
