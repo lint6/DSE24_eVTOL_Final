37b47e610f12806cf09e6e88317a00941f9818ab
lintongxuxu <xulintong3@gmail.com>
2025-01-17 11:31:47 +0100

    updates

--- FlightModel/Newtonian/component.py
+++ FlightModel/Newtonian/component.py
@@ -16,23 +16,18 @@ aircraft.py
 '''
 import numpy as np
 from misc import *
+from physics import *
 
 def ExampleFunction(Constant): #the input modify the function that is to be returned
     return lambda variable: variable*Constant #Return a function that can be stored in a variable
 
 '''Verification Functions''' #functions used for verification of the simulation model
 def SCfunc_RotorThrust(R): #Mimic single input (RPM) function for thrust
-    #TODO Finish up Ct calculation
-    area = np.pi * R**2
-    rho = 1.225
-    return lambda state, rpm: area*rho*(SCfunc_RPM2RadSec(rpm)*R)**2*-0.0087
+    return lambda state, control: SCphy_Thrust(state, control, R)
 
 def SCfunc_RotorTorque(R, clockwise): #Mimic single input (RPM) function for torque
-    #TODO Finish up Cq calculation
-    area = np.pi * R**2
-    rho = 1.225
     if clockwise:
-        return lambda state, rpm: area*rho*(SCfunc_RPM2RadSec(rpm)*R)**2*0.0001*R
+        return lambda state, control: SCphy_Torque(state, control, R)
     else:
-        return lambda state, rpm: area*rho*(SCfunc_RPM2RadSec(rpm)*R)**2*0.0001*R*-1
+        return lambda state, control: SCphy_Torque(state, control, R) * -1
     
