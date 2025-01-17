e6b9195c9d79c462a7c5bd80087eb3c3adaf64c0
lintongxuxu <xulintong3@gmail.com>
2025-01-17 16:30:12 +0100

    pushshshshshhshshs

--- FlightModel/Newtonian/misc.py
+++ FlightModel/Newtonian/misc.py
@@ -137,5 +137,5 @@ if DEBUG:
     # Example Usage
     velocity = 45  # Example velocity input
     alpha = 30  # Example angle of attack input (degrees)
-    result = SCfunc_CSV_reading(r"DSE24_eVTOL_Final\FlightModel\Newtonian\CSV_rotor_data", r"C_T_interpolated_iter1.csv", velocity, alpha)
+    result = SCfunc_CSV_reading(r"FlightModel\Newtonian\CSV_rotor_data", r"C_T_interpolated_iter4.csv", velocity, alpha)
     print("interpolated result is :", result) 
