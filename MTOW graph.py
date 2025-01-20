45f91072a658f1d9c3e4ef9f4a9a3310748f7ca9
jorrit01 <101389718+jorrit01@users.noreply.github.com>
2025-01-20 16:08:57 +0100

    Push to switch branch

--- MTOW graph.py
+++ MTOW graph.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 iterations = [1,2,3,4,5,6,7]
 weights = [718.89,709.63,953.15,765.13,822.17,832.03,839.24]
+max_powers = [97.64,96.38,88.03,68.72,79.86,83.53,85.51]
+cruise_powers = [61.85,61.12,42.71,35.25,42.46,42.96,44.23]
 
 plt.figure(figsize=(10, 6))
 plt.scatter(iterations, weights, color='blue')
@@ -12,6 +14,19 @@ plt.title(f"Iteration Weight Progression")
 plt.grid(color='gray', linestyle=':', linewidth=0.5)
 plt.xlabel("Iteration Number")
 plt.ylabel("MTOW [kg]")
+
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plt.scatter(iterations, max_powers, label='Max Power', color='red')
+plt.scatter(iterations, cruise_powers, label='Cruise Power', color='blue')
+
+plt.title(f"Iteration Power Progression")
+plt.legend(loc='upper right', fontsize='large')
+plt.grid(color='gray', linestyle=':', linewidth=0.5)
+plt.xlabel("Iteration Number")
+plt.ylabel("Power [kW]")
 
 plt.tight_layout()
 plt.show()
